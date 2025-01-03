53e02fabe1188170524de1ac5b284ae58340050c
f104a <f104a@f104a.io>
2025-01-03 23:32:54 +0100

    Revert "[HOTFIX] [qs] [qs tiles]: remove metric proto"
    This reverts commit b4ac8c1cd5b4f3974819845d40df83d5a67436e0.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -76,6 +76,11 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CRDROID_SETTINGS;
+    }
+
+    @Override
     public BooleanState newTileState() {
         return new BooleanState();
     }
